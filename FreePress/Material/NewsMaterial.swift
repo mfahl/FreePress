a454d54aae89efc9cbc113f671e68dfe2291d80f
Martin Fahl <msf@bitplan.com>
2015-10-17 22:57:36 +0200

    changes

--- FreePress/Material/NewsMaterial.swift
+++ FreePress/Material/NewsMaterial.swift
@@ -6,10 +6,12 @@
 //  Copyright © 2015 MartinMarc. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
 class NewsMaterial {
 	var title: String
 	var text: String
+	var image: UIImage?
 	var author: AuthorMaterial?
 
 	init(title: String, text: String) {
@@ -20,5 +22,13 @@ class NewsMaterial {
 	init(json: NSDictionary) {
 			self.title = json["title"] as! String
 			self.text = json["text"] as! String
+			if let imageString = json["image"] as? String {
+				if let url = NSURL(string:imageString) {
+					if let data = NSData(contentsOfURL: url) {
+						self.image = UIImage(data: data)
+					}
+				}
+			}
+		
 	}
 }
