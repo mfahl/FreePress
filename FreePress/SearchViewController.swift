b2e4ec299c73837472a7f5cad4d57dd3d7a0f7c6
Martin Fahl <msf@bitplan.com>
2015-10-17 15:41:02 +0200

    adds continents

---
+++ FreePress/SearchViewController.swift
@@ -0,0 +1,55 @@
+//
+//  SearchViewController.swift
+//  FreePress
+//
+//  Created by Martin on 17.10.15.
+//  Copyright © 2015 MartinMarc. All rights reserved.
+//
+
+import UIKit
+class SearchViewController: UITableViewController {
+	var detailViewController: DetailViewController? = nil
+	var continents = [String]()
+
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		continents.append("Europe")
+		continents.append("Asia")
+		continents.append("Africa")
+	}
+
+
+	// MARK: - Table View
+
+	override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+		return 1
+	}
+
+	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return continents.count
+	}
+
+	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+
+		let continent = continents[indexPath.row]
+		cell.textLabel!.text = continent
+		return cell
+	}
+
+	override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+		// Return false if you do not want the specified item to be editable.
+		return true
+	}
+
+	override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+		if editingStyle == .Delete {
+		    continents.removeAtIndex(indexPath.row)
+		    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+		} else if editingStyle == .Insert {
+		    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+		}
+	}
+
+}
